d1f7faab4baaeb0b0b862623b5ccc1f9aa16b445
Renato <rkauric@vvg.hr>
2022-12-20 12:11:23 +0100

    Added case when no input is present

--- src/assets/components/AlertPopup.tsx
+++ src/assets/components/AlertPopup.tsx
@@ -5,8 +5,21 @@ import { motion } from "framer-motion";
 const AlertPopup = ({ error }: { error: string }) => {
 
   const [showAlert, setShowAlert] = useState(true);
-  const errorType = error === 'success' ? 'success' : 'error';
-  const errorText = error === 'success' ? 'Message successfully copied to clipboard!' : 'Something went wrong, please try again.';
+  const errorType = error === ('no-input' || 'error') ? 'error' : 'success';
+
+  switch (error) {
+    case 'no-input':
+      var errorText = 'Please enter a message to smarten it.';
+      break;
+    case 'error':
+      var errorText = 'Something went wrong, please try again.';
+      break;
+    case 'success':
+      var errorText = 'Message successfully copied to clipboard!';
+      break;
+    default:
+      var errorText = 'Something went wrong, please try again.';
+  }
 
   setTimeout(() => {
     setShowAlert(false);
@@ -15,7 +28,7 @@ const AlertPopup = ({ error }: { error: string }) => {
   return (
     showAlert ? (
       <motion.div initial={{ y: -100 }} animate={{ y: 0 }} exit={{ y: -100 }} className="w-full lg:w-1/2">
-        <Alert severity={errorType}>
+        <Alert severity={errorType} variant="filled" sx={{ borderRadius: 0 }}>
           {errorText}
         </Alert>
       </motion.div>
