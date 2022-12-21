6dc16038969f46a06b36cefcd40602e601777a66
Renato <rkauric@vvg.hr>
2022-12-21 11:52:25 +0100

    Removed unused code

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
